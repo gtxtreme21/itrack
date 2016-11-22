55f775c9aa5adc0b76c2d138cff5449da3671bc7
Gary Tipton <gary.tipton21@gmail.com>
2016-11-21 18:42:35 -0600

    commit changes to show entered from and to location bar codes

--- app/src/main/java/com/aa/itrack/MainActivity.java
+++ app/src/main/java/com/aa/itrack/MainActivity.java
@@ -6,12 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<String> fromToList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fromToList = new ArrayList<String>();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -25,6 +32,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        EditText toLocation = (EditText) findViewById(R.id.toLocation);
+        fromLocationChangeEvent(toLocation);
+        String toLocText = toLocation.getText().toString();
+
+        EditText fromLocation = (EditText) findViewById(R.id.fromLocation);
+        //Set focus in from location text field
+        fromLocation.requestFocus();
+        fromLocationChangeEvent(fromLocation);
+        String fromLocText = fromLocation.getText().toString();
+    }
+
+    //TODO: We will have to change this to a simple on change event and then set focus in the next input field.
+    //TODO: This is only here to test the UI. Gary Tipton
+    private void fromLocationChangeEvent(final EditText fromLocation) {
+        fromLocation.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    String newValue = fromLocation.getText().toString();
+                    if (null != newValue && newValue.length() > 0) {
+                        fromToList.add(newValue);
+                        TextView fromToLocation = (TextView) findViewById(R.id.fromToLocation);
+                        fromToLocation.setText(concatListItems(fromToList));
+                        //Toast.makeText(getApplicationContext(), "unfocus", 2000).show();
+                    }
+                }
+            }
+
+            private String concatListItems(List<String> list) {
+                String concatString = "";
+                for (String item: list) {
+                    if (null != item) {
+                        concatString += item + "\n";
+                    }
+                }
+                return concatString;
+            }
+        });
     }
 
 }
